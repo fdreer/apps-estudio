79c9c54c743e86ebfb5092b64c3ef7cf2ccf0c71
fdreer <francodreer@gmail.com>
2024-11-20 18:37:12 -0300

    Security

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -5,8 +5,8 @@ import { supabase } from '@/lib/supabase'
 
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const formData = await request.formData()
-    const email = formData.get('email')?.toString()
-    const password = formData.get('password')?.toString()
+    const email = formData.get('email')?.toString().trim()
+    const password = formData.get('password')?.toString().trim()
 
     if (!email || !password) {
         return new Response('Email and password are required', { status: 400 })
@@ -24,17 +24,17 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const { access_token, refresh_token } = data.session
     cookies.set('sb-access-token', access_token, {
         httpOnly: true,
-        secure: import.meta.env.PROD,
+        secure: true,
         sameSite: 'strict',
-        maxAge: 1000 * 60 * 60,
+        maxAge: 60 * 60,
         path: '/', // Disponible en todo el dominio
         domain: import.meta.env.PROD ? '.francodreer.com' : undefined // Incluye subdominios
     })
     cookies.set('sb-refresh-token', refresh_token, {
         httpOnly: true,
-        secure: import.meta.env.PROD,
+        secure: true,
         sameSite: 'strict',
-        // maxAge: 1000 * 60 * 60,
+        maxAge: 60 * 60,
         path: '/', // Disponible en todo el dominio
         domain: import.meta.env.PROD ? '.francodreer.com' : undefined // Incluye subdominios
     })
